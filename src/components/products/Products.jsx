22062e0722521b0c10b0ddca876bbbe36172aa4e
adnanali000 <adnan.museel123@gmail.com>
2022-03-24 10:33:40 +0500

    product card work done

---
+++ src/components/products/Products.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import { Grid } from '@material-ui/core'
+import Product from './product/Product'
+
+const products = [
+  { id: 1, name: 'charger', description: 'fast charger.', image:'https://www.ubuy.com.tr/productimg/?image=aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL0kvNTFnRkRPQ2orclMuX0FDX1NMMTUwMF8uanBn.jpg', price: '$10' },
+  { id: 2, name: 'handfree', description: 'Good music quality.',image:'https://www.buykarlo.pk/wp-content/uploads/2020/07/iphone_x_lightning_handsfree_original.jpg', price: '$7' },
+]
+
+const Products = () => {
+  return (
+    <main>
+      <Grid container justify='center' spacing={4}>
+        {products.map((product) => (
+          <Grid item key={product.id} xs={12} s={6} md={4} lg={3} >
+            <Product product={product} />
+          </Grid>
+        ))}
+
+      </Grid>
+    </main>
+  )
+}
+
+export default Products
