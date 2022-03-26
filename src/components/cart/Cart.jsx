30e9feecadbb65f5177eb11eac7dbef5cdb2ba72
adnanali000 <adnan.museel123@gmail.com>
2022-03-26 12:06:12 +0500

    cart item work done now moving toward react router dom work

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Container,Typography,Grid,Button} from '@material-ui/core'
 import useStyles from './styles'
+import CartItem from './cartItem/CartItem'
 
 const Cart = ({cart}) => {
     // const isEmpty = !cart.line_items.length;
@@ -15,7 +16,7 @@ const Cart = ({cart}) => {
             <Grid container spacing={3}>
                 {cart.line_items.map((item)=>(
                     <Grid item xs={12} sm={3} key={item.id}>
-                        <div>{item.name}</div>
+                        <CartItem item={item} />
                     </Grid>
                 ))}
             </Grid>
@@ -38,7 +39,7 @@ const Cart = ({cart}) => {
   return (
     <Container>
         <div className={classes.toolbar} />
-        <Typography className={classes.title} variant="h3">Your Shopping Cart</Typography>
+        <Typography className={classes.title} variant="h4" gutterBottom>Your Shopping Cart</Typography>
         {!cart.line_items.length ? <EmptyCart /> : <FilledCart />}
     </Container>
   )
